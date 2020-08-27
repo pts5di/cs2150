86d5ad74f7b5f36b4e644f00c8f53fe86e914ba1
Paul Stevans <pts5di@gmail.com>
2020-08-27 13:48:01 -0700

    Solution for lab03 IN PROGRESS

---
+++ lab03/postfixCalculator.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+int main() {
+  string myString;
+  stack<int> myStack;
+  int myInt1, myInt2;
+  
+  while(cin << myString) {
+    if(func == "+") {
+      myInt1 = myStack.top();
+      myStack.pop();
+      myInt2 = myStack.top();
+      myStack.pop();
+
+      myStack.push(myInt1 + myInt2);
+    }
+    else if(func == "-") {
+      myInt1 = myStack.top();
+      myStack.pop();
+      myInt2 = myStack.top();
+      myStack.pop();
+
+      myStack.push(myInt1 - myInt2);
+    }
+    else {
+      myStack.push(stoi(myString));
+      
---
+++ lab03/postfixCalculator.h
@@ -0,0 +1,13 @@
+#ifndef POSTFIX_C
+#define POSTFIX_C
+
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+
+
+
+
+#endif
